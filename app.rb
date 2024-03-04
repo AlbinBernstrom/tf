f60699c18edd422ef9f208c9fc34ccb7f922cedb
hoppcorn <albin.bernstrom@elev.ga.ntig.se>
2024-03-04 14:49:39 +0100

    jag lade till lite layout för att skapa karaktärer och lite annat med att registrera och sånt

--- app.rb
+++ app.rb
@@ -4,7 +4,24 @@ require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 
+enable :sessions
+
 get('/') do 
     slim(:home)
 end
 
+get('/register') do
+    slim(:register)
+end
+
+get('/login') do
+    slim(:login)
+end
+
+get('/character_creator') do 
+    slim(:character_creator)
+end
+
+get('/character/new') do
+  slim(:"character/new")
+end
