355f3d55de638c33e077a83a77a6d4e22115dc1a
hoppcorn <albin.bernstrom@elev.ga.ntig.se>
2024-04-11 16:36:31 +0200

    Glömt att pusha senaste lektionerna.

--- app.rb
+++ app.rb
@@ -18,10 +18,50 @@ get('/login') do
     slim(:login)
 end
 
-get('/character_creator') do 
-    slim(:character_creator)
+get('/characters') do 
+    db = SQLite3::Database.new("db/dungeans_database.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM characters")
+    p result
+    slim(:"characters/index",locals:{characters:result})
+
 end
 
-get('/character/new') do
-  slim(:"character/new")
+get('/characters/new') do
+  slim(:"characters/new")
 end
+
+post('/characters/new') do 
+    name = params[:name]
+    user_id = params[:user_id].to_i
+    p "Vi fick in datan #{name} #{user_id}"
+    db = SQLite3::Database.new("db/dungeans_database.db")
+    db.execute("INSERT INTO characters (name, user_id) VALUES (?,?)", name, user_id)
+    redirect('/characters')
+end
+
+post('/characters/:id/delete') do
+    id = params[:id].to_i
+    db = SQLite3::Database.new("db/dungeans_database.db")
+    db.execute("DELETE FROM characters WHERE id = ?",id)
+    redirect('/characters')
+end
+
+get('/characters/:id/edit') do
+    id = params[:id].to_i
+    db = SQLite3::Database.new("db/dungeans_database.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM characters WHERE id = ?",id).first
+    p "hej"
+    slim(:'/characters/edit', locals:{result:result})
+end
+
+post('/characters/:id/update') do
+    id = params[:id].to_i
+    name = params[:name]
+    user_id = params[:user_id].to_i
+    db = SQLite3::Database.new("db/dungeans_database.db")
+    db.execute("UPDATE characters SET name=?,user_id=? WHERE id = ?", name,user_id,id) 
+    redirect('/characters')
+end
+
